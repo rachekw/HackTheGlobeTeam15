5827c5682579bc9c80b026433ce3a80421e043be
rwinterw <rache.wong@mail.utoronto.ca>
2020-03-14 15:29:07 -0400

    added data files and code for screening questions

---
+++ Data.py
@@ -0,0 +1,8 @@
+global frame_size
+frame_size = '800x600'
+
+class data():
+    def __init__(self, name):
+        self.name = name
+
+
--- Main.py
+++ Main.py
@@ -2,11 +2,14 @@ import tkinter as tk
 from tkinter.ttk import *
 from PIL import Image, ImageTk
 
-#for employee evaluation
-from Evaluate import *
-from Rank import *
+#holding all the global variables
 from Data import*
-from important_vars import *
+global img
+from urllib.request import urlopen
+import io
+import base64
+
+
 
 
 class StartingFrame():
@@ -14,47 +17,89 @@ class StartingFrame():
         self.top=top
         self.top.geometry(frame_size)
         self.top.configure(background = 'SkyBlue1')
-
         self.top.title("Child Assessment Tool")
-
         self.frame=tk.Frame(self.top, bg = 'SkyBlue1')
         self.frame.pack()
 
         lbl = tk.Label(self.frame, text="Welcome to CATS", font=("Arial Bold", 15), bg='SkyBlue1')
         lbl.pack()
 
-        select = tk.Label(self.frame, text="Hello, my name is Puddles, what is your name?", font=("Comic Sans MS", 15), bg = 'SkyBlue1')
-        select.pack()
+        select = tk.Label(self.frame, text="Hello, my name is 'Puddles', what is your name?", font=("Comic Sans MS", 15), bg = 'SkyBlue1')
+        select.pack(pady = 20)
 
         txt = tk.Entry(self.frame, width = 10)
         txt.pack()
 
+        self.name = None
+        
         #define a function to respond when an option is selected
         def clicked():
-            select = tk.Label(self.frame, text="It's nice to meet you " + str(txt.get()), font=("Comic Sans MS", 15), bg = 'SkyBlue1')
-            select.pack(side = BOTTOM)
 
-            canvas = Canvas(self.frame, width = 300, height = 300)
-            img = Image.open(file = 'CAT.png')
-            render = ImageTk.PhotoImage(img)
+            self.name = txt.get()
+            select = tk.Label(self.frame, text="It's nice to meet you, " + str(txt.get()) + '!', font=("Comic Sans MS", 15), bg = 'SkyBlue1')
+            select.pack()
 
-            image = Label(self, image = render)
-            image.pack()
-            # canvas.pack()
+            friend = tk.Label(self.frame, text="Would you like to be my friend?", font=("Comic Sans MS", 15), bg = 'SkyBlue1')
+            friend.pack()
+
+            yes = tk.Button(self.frame, text="YES", font = ('Comic Sans MS', 10), command=Play_Yes, bg = 'White')
+            yes.pack(side= tk.TOP) 
+
+            no = tk.Button(self.frame, text="NO", font = ('Comic Sans MS', 10), command=Play_No, bg = 'White')
+            no.pack(side = tk.TOP, pady = 5) 
+
+        btn = tk.Button(self.frame, text="Enter", font = ('Comic Sans MS', 10), command=clicked, bg = 'White')
+        btn.pack(pady = 5)  
+
+
+        def Play_Yes():
+            self.frame.destroy()
+            response = True
+            Start_Playing(root, response)
+
+        def Play_No():
+            self.frame.destroy()
+            response = False
+            Start_Playing(root, response)
+
+########################################YES##################################################################
+class Start_Playing():
+    def __init__(self, top, response):
+        self.top=top
+        self.top.geometry(frame_size)
+        self.top.configure(background = 'SkyBlue2')
+        self.top.title("Child Assessment Tool")
+        self.frame=tk.Frame(self.top, bg = 'SkyBlue2')
+        self.frame.pack()
+        self.response = response
+
+        def play_game():
+            lbl = tk.Label(self.frame, text="I live in a house with my mother and father, I have three siblings: Christina, Sally, and Fumi", font=("Comic Sans MS", 15), bg='SkyBlue1')
+            lbl.pack(pady = 10)
+
+            #display images:
+
+            lbl_1 = tk.Label(self.frame, text = "Who do you live with?", font=("Comic Sans MS", 15), bg='SkyBlue2')
+            lbl_1.pack(pady = 5)
+
             
-        btn = tk.Button(self.frame, text="Enter", command=clicked)
-        btn.pack()
-                
+            
 
-    def go2eval(self):
-        self.frame.destroy()
-        EvaluateEmployeeStartScreen(root)
+        if not (response):
+            lbl = tk.Label(self.frame, text="That's okay, sometimes it's scary for me to meet new people,\n what if we got to know each other a bit better?", font=("Comic Sans MS", 15), bg='SkyBlue2')
+            lbl.pack(pady = 10)
+            btn = tk.Button(self.frame, text = 'Ok', font = ('Comic Sans MS', 10), command=play_game, bg = 'White')
+            btn.pack(pady=5)
 
-    def go2rank(self):
-        self.frame.destroy()
-        RankEmployees(root)
-        
+        else:
+            lbl = tk.Label(self.frame, text="I'm glad that we can be friends, would you like to play with me?", font=("Comic Sans MS", 15), bg='SkyBlue2')
+            lbl.pack(pady=10)
+            btn = tk.Button(self.frame, text = 'Lets go!', font = ('Comic Sans MS', 10), command=play_game, bg = 'White')
+            btn.pack(pady=5)
 
+
+
+    
 #main function
 root=tk.Tk()
 DT=StartingFrame(root)
